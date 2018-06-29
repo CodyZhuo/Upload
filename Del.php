62ad778980ab7bf0bca00877a6a7869afb3b01d6
CodyZhuo <33078893+CodyZhuo@users.noreply.github.com>
2018-06-30 02:59:51 +0800

    正在解决字符串问题

--- Del.php
+++ Del.php
@@ -15,12 +15,12 @@ $url2 = explode("=",$url);
 $conn = pg_connect("host=localhost port=5432 dbname=Sanhuanpao user=postgres password=lily520");
 $result= pg_query ($conn,"SELECT picture  FROM plants WHERE name_id = '$url2[1]'");
 $arr = pg_fetch_all($result);
-$arrStr = json_encode($arr[0]['picture']);//已经是字符串,因为如果直接把字符串分开的话，会连 picture:这类的都得到
-$arr2 = explode(",",$arrStr);//提取各个地址
+// $arrStr = json_encode($arr[0]['picture']);//已经是字符串,因为如果直接把字符串分开的话，会连 picture:这类的都得到
+// $arr2 = explode(",",$arrStr);//提取各个地址
 
 var_dump($arr);
 foreach ($picture as $key => $value) {
-$Rarr = $arr2[$value];
+$Rarr = $arr[0]['picture'][$value];
 // var_dump($Rarr);
 // echo $Rarr;
 
